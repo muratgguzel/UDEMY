9e773e18746d3a5addde72d127513ad82344b76e
Murat Guzel <muratgguzel77@yahoo.com>
2024-07-01 20:35:27 -0500

    7/11

--- Net8Arch/Section4/ToDoApi/TodoApi/Program.cs
+++ Net8Arch/Section4/ToDoApi/TodoApi/Program.cs
@@ -1,6 +1,57 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Add Dependecny Injection Parameters
+
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/todoitems", async (TodoDb db)=> await db.Todos.ToListAsync());
+
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) => await db.Todos.FindAsync(id));
+
+app.MapPost("/todoitems", async (TodoItem todo, TodoDb db) =>
+{ 
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+    return Results.Created($"/todoitems/{todo.Id}",todo);
+});
+
+app.MapPut("/todoitems/{id}", async (int id, TodoItem inputTodo, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+    if (todo == null) return Results.NotFound();
+    todo.name = inputTodo.name;
+    todo.IsComplete = inputTodo.IsComplete;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+
+});
+   
+app.MapDelete("/todoitems/{id}" , async (int id, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+    if (todo == null) return Results.NotFound();
+    
+    db.Todos.Remove(todo);
+    await db.SaveChangesAsync();
+    return Results.NoContent() ;
+
+} );
+
+
+
+
+
+
+
+
+    
+
+
 
 app.Run();
---
+++ Net8Arch/Section4/ToDoApi/TodoApi/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace TodoApi
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+
+        public string name { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
